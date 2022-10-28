0de4c6f3a12ea300a1af838f3655a1a936659941
Pamilerin Ajayi <pajayi@vatebra.com>
2022-10-28 19:50:55 +0100

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using HNGTaskOne.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HNGTaskOne.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public List<User> Get()
+        {
+            List<User> users = new List<User>();
+            users.Add(new User("Pamilerin", true, 25, "Pamilerin is a .NET developer and his goal is to be the best at what he does"));
+
+            return users;
+
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HNGTaskOne.Models
+{
+    public class User
+    {
+        public string SlackUsername { get; set; }
+        public bool Backend { get; set; }
+        public int Age { get; set; }
+        public string Bio { get; set; }
+
+        public User()
+        {
+        }
+
+        public User(string slackUsername, bool backend, int age, string bio)
+        {
+            this.SlackUsername = slackUsername;
+            this.Backend = backend;
+            this.Age = age;
+            this.Bio = bio;
+        }
+    }
+}
